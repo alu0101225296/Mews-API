97dfc35a2bc9b72154a5a7bb351f46026f0fb3d0
pbande <alu0101225296@ull.edu.es>
2022-07-01 03:32:26 +0100

    :bug: getNews returning lastNews bug

--- app-api/src/routes/phone-app.js
+++ app-api/src/routes/phone-app.js
@@ -32,13 +32,13 @@ router.get('/api/news/:artistId', (req, res) => {
 			req.params.artistId,
 			req.query.limit,
 			req.query.start
-		).then((news) => {
-			res.json(news);
+		).then((news, lastNews) => {
+			res.json({ news, lastNews });
 		});
 	} else {
 		getNewsByArtistUsingLimit(req.params.artistId, req.query.limit).then(
-			(news) => {
-				res.json(news);
+			(news, lastNews) => {
+				res.json({ news, lastNews });
 			}
 		);
 	}
